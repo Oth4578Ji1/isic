585210942f2c32b62c3bd6c70938c1213018e76b
Othmane <chehbouni.edaoudi123@gmail.com>
2024-08-24 16:15:18 +0100

    edit settings

--- isic/settings.py
+++ isic/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bxzfsvjjcw@i(o)a$w^m!i+-bs1#^%zw%dbrzrzaa+utd6^+^g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
