6d2eb1efa900d78c63fe70379a14c3c12b93ec61
Othmane <chehbouni.edaoudi123@gmail.com>
2024-08-24 13:57:59 +0100

    git push origin main

--- isic/settings.py
+++ isic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bxzfsvjjcw@i(o)a$w^m!i+-bs1#^%zw%dbrzrzaa+utd6^+^g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
